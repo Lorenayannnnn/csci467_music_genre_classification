19fde155e5dc116612c8385576e00cc4824aa76d
Lorenayannnnn <lorenayan@qq.com>
2023-03-13 16:25:34 -0700

    add train script

---
+++ HubertForGenreClassification/train.sh
@@ -0,0 +1,6 @@
+python3 run_music_genre_classification.py \
+  --outputs_dir outputs/ \
+  --data_dir ../data/genres_original/ \
+  --data_split_txt_filepath ../data_split.txt \
+  --batch_size 8 \
+  --num_epochs 10
