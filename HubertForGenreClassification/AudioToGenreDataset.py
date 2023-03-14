59d4052433657eb8a393f1a5d85676857bd2212d
Lorenayannnnn <lorenayan@qq.com>
2023-03-13 17:36:02 -0700

    update model (freeze hubert and only train classifier)

--- HubertForGenreClassification/AudioToGenreDataset.py
+++ HubertForGenreClassification/AudioToGenreDataset.py
@@ -20,13 +20,13 @@ genre_2_index = {
 index_2_genre = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
 
 class AudioToGenreDataset(Dataset):
-    def __init__(self, root_dir, dataframe, feature_extractor, max_length):
+    def __init__(self, root_dir, dataframe, feature_extractor, sample_rate):
         # dataframe = {"audio_filename": [], "label": []}
         self.data_dict = dataframe
         self.root_dir = root_dir
         # Hubert feature extractor
         self.feature_extractor = feature_extractor
-        self.max_length = max_length
+        self.sample_rate = sample_rate
 
     def __len__(self):
         return len(self.data_dict)
@@ -40,29 +40,27 @@ class AudioToGenreDataset(Dataset):
         label = genre_2_index[self.data_dict.loc[idx, "label"]]
 
         # Convert audio to array of float
-        processed_audio_data = convert_audio_to_float_input_values(audio_file_name)
+        processed_audio_data = convert_audio_to_float_input_values(audio_file_name, self.sample_rate)
 
         inputs = self.feature_extractor(processed_audio_data, sampling_rate=self.feature_extractor.sampling_rate,
-                                        max_length=self.max_length, truncation=True,)
+                                        max_length=self.sample_rate, truncation=True)
 
         inputs["label"] = torch.tensor(label, dtype=torch.long)
         return inputs
 
 
 # Reference: https://stackoverflow.com/questions/33682490/how-to-read-a-wav-file-using-scipy-at-a-different-sampling-rate
-def convert_audio_to_float_input_values(audio_file_name: str):
+def convert_audio_to_float_input_values(audio_file_name: str, input_sample_rate: int):
     """
     Read input audio file and convert the file into the required sample rate
     :return audio: array of float
     """
-    # Required by Hubert
-    REQUIRED_SAMPLERATE = 16000
     sample_rate, audio = wavfile.read(audio_file_name)
 
-    if sample_rate != REQUIRED_SAMPLERATE:
+    if sample_rate != input_sample_rate:
         duration = audio.shape[0] / sample_rate
         time_old = np.linspace(0, duration, audio.shape[0])
-        time_new = np.linspace(0, duration, int(audio.shape[0] * REQUIRED_SAMPLERATE / sample_rate))
+        time_new = np.linspace(0, duration, int(audio.shape[0] * input_sample_rate / sample_rate))
         interpolator = interpolate.interp1d(time_old, audio.T)
         audio = interpolator(time_new).T
 
--- HubertForGenreClassification/HubertForGenreClassification.py
+++ HubertForGenreClassification/HubertForGenreClassification.py
@@ -1,16 +1,27 @@
-
+import torch
 from torch import nn
-from transformers import AutoModelForAudioClassification
-
-from AudioToGenreDataset import genre_2_index, index_2_genre
+from transformers import HubertModel, HubertConfig
 
 
 class HubertForGenreClassification(nn.Module):
 
     def __init__(self, model_name):
         super(HubertForGenreClassification, self).__init__()
-        self.model = AutoModelForAudioClassification.from_pretrained(model_name, num_labels=10,
-                                                                     label2id=genre_2_index, id2label=index_2_genre)
+        self.config = HubertConfig()
+        self.hubert_model = HubertModel(self.config)
+
+        self.projector = nn.Linear(in_features=self.config.hidden_size, out_features=int(self.config.hidden_size / 2))
+        self.classifier = nn.Linear(in_features=int(self.config.hidden_size / 2), out_features=10)
+
 
     def forward(self, inputs):
-        return self.model(inputs)
+        # Freeze the model (use as feature extractor)
+        with torch.no_grad():
+            hidden_states = self.hubert_model(inputs).last_hidden_state
+            # Get hidden state of last token of the entire sequence
+            last_token_hidden = hidden_states[:, -1, :]
+
+        projector_outputs = self.projector(last_token_hidden)
+        outputs = self.classifier(projector_outputs)
+
+        return outputs
