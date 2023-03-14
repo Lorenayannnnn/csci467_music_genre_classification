96be4dbb714cad8ae99c62de47918f39325c6b86
Lorenayannnnn <lorenayan@qq.com>
2023-03-13 18:40:49 -0700

    update train script

--- HubertForGenreClassification/train.sh
+++ HubertForGenreClassification/train.sh
@@ -1,8 +1,10 @@
 python3 run_music_genre_classification.py \
-  --outputs_dir outputs/hubert_rate_16000_freeze_extractor/ \
+  --outputs_dir ./outputs/hubert_rate_16000_freeze_extractor/ \
+  --do_eval \
+  --do_test \
   --data_dir ../data/genres_original/ \
   --data_split_txt_filepath ../data_split.txt \
   --batch_size 32 \
-  --num_epochs 10 \
-  --val_every 2 \
+  --num_epochs 20 \
+  --val_every 10 \
   --sample_rate 16000 \
